63080812bdcae6e3fbf598fe5b04f903c6ccdbe3
zhengzangw <zhengzangw@163.com>
2019-01-15 14:46:22 +0800

    Add argpars

--- models.py
+++ models.py
@@ -3,19 +3,17 @@ from log_utils import get_logger
 LOGGER = get_logger("models")
 
 def get_model(name,vocab_size,embedding_matrix,input_length):
-    if name=="cnn_model":
-        return cnn_model(vocab_size,embedding_matrix,input_length)
-    elif name=="new_cnn_model":
-        return new_cnn_model(vocab_size,embedding_matrix,input_length)
-    elif name=="lstm_model":
-        return lstm_model(vocab_size,embedding_matrix,input_length)
-    elif name=="zzw_model":
-        return zzw_model(vocab_size, embedding_matrix, input_length)
+    if name=="demo_cnn":
+        return demo_cnn(vocab_size,embedding_matrix,input_length)
+    elif name=="two_level_cnn":
+        return two_level_cnn(vocab_size,embedding_matrix,input_length)
+    elif name=="two_level_lstm":
+        return two_level_lstm(vocab_size,embedding_matrix,input_length)
+    else:
+        LOGGER.error("no such model: {}".format(name))
 
 
-def cnn_model(vocab_size,embedding_matrix,input_length):
-    # maxlen=200: 72%
-    # maxlen=2000: 75%
+def demo_cnn(vocab_size,embedding_matrix,input_length):
     model = keras.Sequential()
     e = keras.layers.Embedding(vocab_size, 50, weights=[embedding_matrix], input_length=input_length, trainable=False)
     model.add(e)
@@ -25,8 +23,10 @@ def cnn_model(vocab_size,embedding_matrix,input_length):
     model.add(keras.layers.Dropout(0.2))
     model.add(keras.layers.Dense(4, activation='sigmoid'))
     return model
+# maxlen=200: 72%
+# maxlen=2000: 75%
 
-def new_cnn_model(vocab_size,embedding_matrix,input_length):
+def two_level_cnn(vocab_size,embedding_matrix,input_length):
     model = keras.Sequential()
     e = keras.layers.Embedding(vocab_size, 50, weights=[embedding_matrix], input_length=input_length, trainable=False)
     model.add(e)
@@ -40,27 +40,13 @@ def new_cnn_model(vocab_size,embedding_matrix,input_length):
     model.add(keras.layers.Dense(4, activation='sigmoid'))
     return model
 
-def lstm_model(vocab_size,embedding_matrix,input_length):
+def two_level_lstm(vocab_size,embedding_matrix,input_length):
     model = keras.Sequential()
     e = keras.layers.Embedding(vocab_size, 50, weights=[embedding_matrix], input_length=input_length, trainable=False)
     model.add(e)
-    model.add(keras.layers.LSTM(20, return_sequences=True))
-    model.add(keras.layers.Dropout(0.2))
-    model.add(keras.layers.Conv1D(16, 5, padding='valid', activation='relu', strides=1))
-    model.add(keras.layers.GlobalMaxPool1D())
-    model.add(keras.layers.Dense(4, activation='sigmoid'))
-    return model
-
-def zzw_model(vocab_size,embedding_matrix,input_length):
-    model = keras.Sequential()
-    e = keras.layers.Embedding(vocab_size, 50, weights=[embedding_matrix], input_length=input_length, trainable=False)
-    model.add(e)
+    model.add(keras.layers.LSTM(50, return_sequences=True))
+    model.add(keras.layers.LSTM(50, return_sequences=True))
     model.add(keras.layers.Dropout(0.5))
-    model.add(keras.layers.Conv1D(64, 200, padding='valid', activation='relu', strides=1))
-    model.add(keras.layers.Dropout(0.5))
-    model.add(keras.layers.Conv1D(64, 200, padding='valid', activation='relu', strides=1))
-    model.add(keras.layers.Dropout(0.5))
-    model.add(keras.layers.LSTM(10, return_sequences=True))
-    model.add(keras.layers.Flatten())
+    model.add(keras.layers.TimeDistributed(keras.layers.Dense(vocab_size/10)))
     model.add(keras.layers.Dense(4, activation='sigmoid'))
     return model
