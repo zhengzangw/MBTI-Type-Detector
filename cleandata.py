95fef8337be2d96da0014c35fea247f598aa39f3
cherry979988 <yeqy15@mails.tsinghua.edu.cn>
2019-01-15 07:39:34 -0800

    add tokenize in data cleaning. v0 original version. v1 tokenized + <return> + emoji, user-level v2 sentence-level

--- cleandata.py
+++ cleandata.py
@@ -1,12 +1,15 @@
 # Preworks lib
 import pandas as pd
+import numpy as np
+import re
+from stanza.nlp.corenlp import CoreNLPClient
 
 # read from file
 def input_csv(file_name):
     df = pd.read_csv(file_name)
     # Get rid of first and last character
     for t in range(len(df)):
-        df['posts'][t] = df['posts'][t][1:-1]
+        df['posts'][t] = df['posts'][t].strip("'")
     df.head()
     return df
 
@@ -19,6 +22,22 @@ def get_the_label(df):
     for t in range(len(MBTI)):
         df[MBTI[t]] = [1 if df['type'][i][t]==MBTI_pos[t] else 0 for i in range(len(df))]
 
+def tokenize(df):
+    parser = CoreNLPClient(default_annotators=['ssplit', 'tokenize'], server='http://localhost:9000')
+    parsed = []
+    for item in df['posts']:
+        temp = []
+        for sentence in item.strip().split('|||'):
+            try:
+                result = parser.annotate(sentence)
+                tokens = []
+                for i in range(len(result.sentences)):
+                    tokens += result.sentences[i].tokens
+                temp.append(' '.join([token.word for token in tokens]))
+            except:
+                print('error', sentence)
+        parsed.append(' <RETURN> '.join(temp))
+    df['posts'] = parsed
 
 # Replace Return
 def deal_with_seperator(df):
@@ -75,6 +94,7 @@ def prework(input_file, output_file = '', is_split_sentence = False, output = Fa
     deal_with_seperator(df)
     deal_with_URL(df)
     deal_with_emoji(df)
+    # tokenize(df)
     # selectable
     if is_split_sentence == True:
         df = split_sentence(df)
@@ -85,9 +105,17 @@ def prework(input_file, output_file = '', is_split_sentence = False, output = Fa
     else:
         return df
 
-# Copy MBTIv0 to MBTIv1 and Split to MBTIv2
+def prework_tokenize(input_file, output_file):
+    df = input_csv(input_file)
+    get_the_label(df)
+    deal_with_URL(df)
+    deal_with_emoji(df)
+    tokenize(df)
+    output_csv(df, output_file)
+
+# Just for Debug or you just want to prework
 if __name__ == '__main__':
-    df = pd.read_csv('./MBTIv0.csv')
-    df.to_csv('./MBTIv1.csv')
+    prework_tokenize('./MBTIv0.csv', './MBTIv1.csv')
+    df = pd.read_csv('./MBTIv1.csv')
     df = split_sentence(df)
     df.to_csv('./MBTIv2.csv')
