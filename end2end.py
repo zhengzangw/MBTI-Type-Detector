e80278ff0b5e4525b4381a612f2d03171ed7d853
zhengzangw <zhengzangw@163.com>
2019-01-16 20:57:39 +0800

    fast commit from zhengzangwei

--- end2end.py
+++ end2end.py
@@ -26,7 +26,7 @@ IS_SEQ = False
 import argparse
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-m", "--model", dest="model", type=str, default="cnn", help="Choose Your Model")
+    parser.add_argument("-m", "--model", dest="model", type=str, default="zzw_lstm", help="Choose Your Model")
     parser.add_argument("-s", "--seq", dest="is_seq", action='store_true', help="Is test on sequence")
     parser.add_argument("-l", "--load", dest="loadpath", type=str, default=None, help="Load Model")
     parser.add_argument("-c", "--classify", dest="classify", type=int, default=4, help="Choose The Classify Method, 4/16")
@@ -211,7 +211,8 @@ if __name__=="__main__":
             assert(0)
         loss_func = 'binary_crossentropy' if CTYPE==4 else 'categorical_crossentropy'
         sgd = keras.optimizers.SGD(lr=0.001, decay=1e-6, momentum=0.9, nesterov=True)
-        model.compile(loss = loss_func, optimizer=sgd, metrics=['accuracy'])
+        adam = keras.optimizers.Adam()
+        model.compile(loss = loss_func, optimizer=adam, metrics=['accuracy'])
 
         # Training
         LOGGER.info("Begin Training")
--- models.py
+++ models.py
@@ -29,17 +29,16 @@ def zzw_cnn(vocab_size,embedding_matrix,input_length, classify_type):
     model.add(keras.layers.Conv1D(128, 5, padding='valid', activation='relu', strides=1))
     model.add(keras.layers.MaxPool1D(35))
     model.add(keras.layers.Flatten())
-    model.add(keras.layers.Dropout(0.5))
     model.add(keras.layers.Dense(128, activation='relu'))
     model.add(keras.layers.Dense(classify_type, activation=final_active_func(classify_type)))
     return model
 
 def zzw_lstm(vocab_size,embedding_matrix,input_length, classify_type):
     model = keras.Sequential()
-    e = keras.layers.Embedding(vocab_size, 50, weights=[embedding_matrix], input_length=input_length, trainable=False)
+    e = keras.layers.Embedding(vocab_size, 50, weights=[embedding_matrix], input_length=input_length, trainable=False,
+                                     batch_input_shape=(32, input_length, 50))
     model.add(e)
-    model.add(keras.layers.CuDNNLSTM(50, return_sequences=True, stateful=True,
-                                     batch_input_shape=(32, input_length, 50)))
+    model.add(keras.layers.CuDNNLSTM(50, return_sequences=True, stateful=True))
     model.add(keras.layers.CuDNNLSTM(50, return_sequences=True, stateful=True))
     model.add(keras.layers.CuDNNLSTM(50, return_sequences=False, stateful=True))
     model.add(keras.layers.Dense(classify_type, activation=final_active_func(classify_type)))
