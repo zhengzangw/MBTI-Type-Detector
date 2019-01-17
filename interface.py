04e295be2fe06ed4f00ef484c2f86212a46f95d7
chenxiaoyu233 <2016060106020@std.uestc.edu.cn>
2019-01-17 16:01:51 +0800

    [new feature] close button

--- interface.py
+++ interface.py
@@ -184,9 +184,17 @@ class Application(tk.Frame):
         self.initPersonalityPic()
         self.initWordGraphButton()
 
+from tkinter import messagebox
+root = tk.Tk()
+def on_closing():
+    if messagebox.askokcancel("Quit", "Do you want to quit?"):
+        root.destroy()
+        exit(0)
 
 if __name__ == '__main__':
     vis.load_model_and_data("demo_cnn.h5")
-    app = Application()                     
+    app = Application(root)
+    root.protocol("WM_DELETE_WINDOW", on_closing)
     app.master.title('Sample application')   
-    app.mainloop()         
+    root.protocol("WM_DELETE_WINDOW", on_closing)
+    root.mainloop()
